3845accaeb3463f2e29409c3ebbd85328c4f50f7
Farid_mr <itmirengineering@gmail.com>
2024-01-26 20:55:51 +0330

    sssss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ["./public/*.html", "./public/js/*.js"],
   theme: {
+      container: {
+      center: true,
+      padding: "2rem"
+    },
+    fontFamily: {
+      "vazir" : "vazir",
+      "vazirBold" : "vazirBold",
+      "nasim" : "nasim"
+    },
     extend: {
-      
+      colors: {
+        primary: "var(--primary)",
+      },
+      screens: {
+        'vmini' : '393px',
+        'mobile' : '500px',
+        'ipad' : '600px',
+        'tablet' : '768px',
+        'laptop' : '992px',
+        'desktop' : '1200px',
+        'large' : '1400px'
+      }
     },
   },
   plugins: [],
